3ae0c2261f1de03dbeb8be31980fafbbd60d8081
Niall Robinson <niall.h.robinson@gmail.com>
2016-04-27 13:13:34 +0100

    added template s3 service

---
+++ services/s3/image-service.js
@@ -0,0 +1,90 @@
+/**
+ * Created by tom on 07/04/16.
+ */
+
+var AWS = require('aws-sdk');
+AWS.config.update({
+    region: "eu-west-1",
+});
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+var uuid = require('node-uuid');
+
+//logging
+var debug = require('debug')('molab-mysky-api:services/molab-mysky-db/image-service');
+
+module.exports = {
+
+    /**
+     * Inserts a user image into the db
+     * @param deviceId - id of device submitting the user ob
+     * @param location - location of device submitting the user ob
+     * @param data - the image file we are putting
+     * @param dt - datetime the image is taken
+     * @param obs - array of professional observations taken from device location
+     * @param fcsts - array of professional forecasts taken from device location
+     * @returns {Promise}
+     */
+    add: function (deviceId, location, dt, data, obs, fcsts) {
+        return new Promise(
+            function (resolve, reject) {
+                debug("adding user image");
+                var params = {
+                    TableName: "image",
+                    Item: {
+                        id: uuid.v4(),
+                        type: "Feature",
+                        geometry: {
+                            type: "Point",
+                            coordinates: [location.latitude, location.longitude]
+                        },
+                        properties: {
+                            deviceId: deviceId,
+                            dt: dt,
+                            obs: obs,
+                            fcsts: fcsts,
+                            data: data
+                        }
+                    },
+                    "ConditionExpression": "attribute_not_exists(id)"
+                };
+                debug("hi from promise")
+                docClient.put(params, function (err, data) {
+                    if (err) {                              // an error occurred
+                        debug(err, err.stack);
+                        reject(err);
+                    }
+                    else {                                  // successful response from db
+                        resolve(data);
+                    }
+                });
+            });
+    }//,
+    // getKeys: function() {
+    //     return new Promise(
+    //         function(resolve, reject){
+    //             debug("getting all keys")
+    //             var params = {RequestItems:
+    //                             {"images":
+    //                                 {Keys: [],
+    //                                 AttributesToGet: ["id"]}}};
+    //             docClient.batchGet(params, function(err, data){
+    //                 if (err) {
+    //                     debug(err, err.stack)
+    //                     reject(err)
+    //                 }
+    //                 else{
+    //                     debug(data)
+    //                     resolve(data)
+    //                 }
+    //             })
+    //         })
+    // },
+    // getImage: function(){
+    //     return new Promise(
+    //         function(resolve, reject){
+    //             debug("getting an image")
+    //             docClient.get()
+    //         })
+
+    // }
+};
---
+++ services/s3/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by tom on 08/04/16.
+ */
+
+exports.userObService = require('./user-ob-service');
+exports.imageService = require('./image-service');
