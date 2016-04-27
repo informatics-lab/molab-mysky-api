aad6e72f0398f92640e85dbe2289ea88de22e7fd
Niall Robinson <niall.h.robinson@gmail.com>
2016-04-27 10:25:37 +0100

    added template image-service

--- routes/image.js
+++ routes/image.js
@@ -2,33 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 var db = require('../services/molab-wtf');
-var validator = require('../validators').userObValidator;
-var debug = require('debug')('molab-wtf:routes/ob');
+var validator = require('../validators').userImageValidator;
+var debug = require('debug')('molab-wtf:routes/image');
 
 /**
- * allow user to post ob.
+ * allow user to post image.
  */
 router.post('/', function(req, res) {
 
-    debug('user ob posted \n', req.body);
+    debug('user image posted \n', req.body);
 
     //validate req payload
     validator.validate(req.body).then(
         function() {
-            debug('user ob valid');
+            debug('user image valid');
 
             //TODO fetch professional obs and fcsts
 
-            //add user ob to db
-            db.userObService.add(req.body.deviceId, req.body.sessionId, req.body.location, req.body.ob, [], []).then(
+            //add user image to db
+            db.userimageService.add(req.body.deviceId, req.body.sessionId, req.body.location, req.body.image, [], []).then(
                 function() {
-                    debug('user ob stored');
+                    debug('user image stored');
                     res.sendStatus(201);
                     return;
                 }
             ).catch(
                 function(err) {
-                    debug('user ob caused server error');
+                    debug('user image caused server error');
                     res.status(500).send(err);
                     return;
                 }
@@ -36,7 +36,7 @@ router.post('/', function(req, res) {
         }
     ).catch(
         function(err) {
-            debug('user ob payload was invalid');
+            debug('user image payload was invalid');
             res.status(400).send({"errors":err});
             return;
         }
