752aa080912609f810f94d9a737feaa17152b657
Niall Robinson <niall.h.robinson@gmail.com>
2016-04-27 10:19:32 +0100

    added image that needs editing

---
+++ routes/image.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../services/molab-wtf');
+var validator = require('../validators').userObValidator;
+var debug = require('debug')('molab-wtf:routes/ob');
+
+/**
+ * allow user to post ob.
+ */
+router.post('/', function(req, res) {
+
+    debug('user ob posted \n', req.body);
+
+    //validate req payload
+    validator.validate(req.body).then(
+        function() {
+            debug('user ob valid');
+
+            //TODO fetch professional obs and fcsts
+
+            //add user ob to db
+            db.userObService.add(req.body.deviceId, req.body.sessionId, req.body.location, req.body.ob, [], []).then(
+                function() {
+                    debug('user ob stored');
+                    res.sendStatus(201);
+                    return;
+                }
+            ).catch(
+                function(err) {
+                    debug('user ob caused server error');
+                    res.status(500).send(err);
+                    return;
+                }
+            );
+        }
+    ).catch(
+        function(err) {
+            debug('user ob payload was invalid');
+            res.status(400).send({"errors":err});
+            return;
+        }
+    );
+
+});
+
+module.exports = router;
